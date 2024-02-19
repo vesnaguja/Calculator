758c03e2882764953fe59a9066bedecbc7642437
Vesna Grujanic <vesnagrujanic@192.168.1.55>
2024-02-19 19:28:05 +0100

    update next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+export function exportPathMap() {
+    return {
+        '/': { page: '/' },
+        // Add other pages as needed
+    };
+}
